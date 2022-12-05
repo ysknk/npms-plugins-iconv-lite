8117305dae24beef633f3374fc395349b4bfb9c3
ysknk <31568245+ysknk@users.noreply.github.com>
2022-12-05 20:37:55 +0900

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# glob-iconv-lite
-glob iconv lite cli
+# glob-del-cli
+
+## Description
+
+delをcli化。globで複数ファイルを指定して削除できるようにしたもの。  
+引数を渡すことでdelを実行することができる。  
+
+## Requirement
+
+* Node.js -> check cmd `node -v`
+
+## Install
+
+```sh
+npm i -D https://github.com/ysknk/glob-del-cli.git
+```
+
+## Usage
+
+### add script in package.json
+
+```json
+{
+  "scripts": {
+    "del": "del"
+  },
+}
+```
+
+```sh
+# check arguments help
+npm run del -- --help
+```
+
+### ex) set options
+
+project root `.delrc.js`  
+or  
+cli `npm run del -- -base "./test/" -src "**/[!_]*.js" -dryRun true
+`
---
+++ index.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+'use strict'
+
+/* @author ysknk */
+
+import { promises as fs } from 'fs'
+import path from 'path'
+import glob from 'glob'
+
+import iconv from 'iconv-lite'
+
+import utils from 'node-package-utilities'
+
+import argv from './lib/arguments.js'
+
+glob.sync(argv.src, {
+  ignore: argv.ignore,
+  cwd: argv.cwd
+}).map((key) => {
+  const filename = key.replace(/\.[^/.]+$/, '');
+  const name = argv.ext ? `${filename}${argv.ext}` : key
+
+  const destpath = argv.dest
+    ? path.resolve(argv.dest, name)
+    : path.resolve(argv.cwd, name)
+
+  // NOTE: watch related path
+  const relpath = argv.rel
+    ? path.resolve(argv.rel, name)
+    : path.resolve(argv.cwd, name)
+
+  ;(async () => {
+    let data = ''
+    try {
+      data = await fs.readFile(relpath)
+    } catch(err) {
+      utils.message.failure(err, {ptime: false})
+    }
+
+    const contents = iconv.decode(data, argv.from)
+    const encodeContents = iconv.encode(contents, argv.to)
+    const result = new Buffer.from(encodeContents)
+
+    await utils.file.makeDir(path.parse(destpath).dir)
+
+    await fs.writeFile(destpath, result)
+
+    utils.message.success(`encode[${argv.from} -> ${argv.to}]: ${destpath}`, {ptime: false})
+  })()
+})
